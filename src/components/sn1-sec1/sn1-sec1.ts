fe502da56189b95626c2874ab4c36e116ebdd4a7
Jormthup <jomtup12@gmail.com>
2018-06-25 15:22:46 +0700

    update location 2

--- src/components/sn1-sec1/sn1-sec1.ts
+++ src/components/sn1-sec1/sn1-sec1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Events,Platform } from 'ionic-angular';
+import { Events } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 /**
  * Generated class for the Sn1Sec1Component component.
@@ -18,15 +18,7 @@ export class Sn1Sec1Component {
   lon: any;
   wathId: any;
   message: any;
-  constructor(private events: Events, public geo: Geolocation, private platform: Platform) {
-    this.platform.ready().then(() => {
-      this.geo.getCurrentPosition().then(resp => {
-        console.log(resp.coords.latitude);
-        console.log(resp.coords.longitude);
-      }).catch(() => {
-        console.log("Error to get location")
-      })
-    });
+  constructor(private events: Events, public geo: Geolocation) {
     console.log('Hello Sn1Sec1Component Component');
     this.text = 'Hello World';
     this.message = "-";
@@ -38,38 +30,27 @@ export class Sn1Sec1Component {
     this.events.publish("sec1Submitted");
 
   }
-  loadMap() {
-    let options = {
-      enableHighAccuracy: true
-  };
-    this.geo.getCurrentPosition(options).then((position) => {
-      //console.log(position);
-      this.lon = position.coords.longitude
-      this.lat = position.coords.latitude;
-    }, (err) => {
-      this.message = "GPS error" + err; 
-    });
-  
-  // startLocation()
-  // {
-  //   let geoOption = {enableHighAccuracy : true};
-  //   try
-  //   {
-  //     this.wathId = this.geo.watchPosition(geoOption).subscribe(data => 
-  //     {
-  //       this.lat = data.coords.latitude;
-  //       this.lon = data.coords.longitude;
-  //     },
-  //     error =>
-  //     {
-  //       this.message = "GPS error" + error; 
-  //     }
-  //   )
-  //   }catch(err)
-  //   {
-  //     alert("Error" + err);
-  //     this.message = "Error" + err;
-  //   }
-  // }
+
+  loadMap()
+  {
+    let geoOption = {enableHighAccuracy : true};
+    try
+    {
+      this.wathId = this.geo.watchPosition(geoOption).subscribe(data => 
+      {
+        this.lat = data.coords.latitude;
+        this.lon = data.coords.longitude;
+      },
+      error =>
+      {
+        this.message = "GPS error" + error; 
+      }
+    )
+    }catch(err)
+    {
+      alert("Error" + err);
+      this.message = "Error" + err;
+    }
+  }
 }
-}
+
